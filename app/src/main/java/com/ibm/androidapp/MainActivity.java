26f758c2aa333f8adb6c2404a0e40090c7dbad0a
Matthew Rattray <ylva@matthews-mbp.austin.ibm.com>
2020-02-06 17:15:40 -0600

    Initial commit.

---
+++ app/src/main/java/com/ibm/androidapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.ibm.androidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setupWebView((WebView) findViewById(R.id.webView));
+    }
+
+    protected void setupWebView(WebView webView) {
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.loadUrl("file:///android_asset/www/index.html");
+    }
+
+}
